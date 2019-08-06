f409f85be8ee5370a2052b9b01822d940007b837
Jefferson Ly <32202953+jeffersonly@users.noreply.github.com>
2019-08-06 16:32:41 -0700

    added projects page

--- js/script.js
+++ js/script.js
@@ -31,4 +31,28 @@ $(document).ready(function () {
             delay: 5000
         }
     });
+    /* Swiper in Projects Section */
+    var swiper = new Swiper('.swiper-proj-container', {
+        effect: 'coverflow',
+        loop: true,
+        centeredSlides: true,
+        slidesPerView: 3,
+        initialSlide: 3,
+        keyboardControl: true,
+        mousewheelControl: true,
+        lazyLoading: true,
+        preventClicks: false,
+        preventClicksPropagation: false,
+        lazyLoadingInPrevNext: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        coverflow: {
+            rotate: 0,
+            stretch: 0,
+            depth: 250,
+            modifier: 1,
+            slideShadows : false,
+        }
+    });
+  
 });
